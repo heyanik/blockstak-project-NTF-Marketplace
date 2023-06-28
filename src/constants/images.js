930098c60af8072946f04ebd1c1b27ae43fb439e
AnikDasPartha58 <Anikdaspartha58@gmail.com>
2023-06-29 01:01:28 +0600

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,27 @@
+import gradient_1 from "../asset/gradient-1.png";
+import gradient_2 from "../asset/gradient-2.png";
+import gradient_3 from "../asset/gradient-3.png";
+import gradient_4 from "../asset/gradient-4.png";
+import gradient_5 from "../asset/gradient-5.png";
+import gradient_6 from "../asset/gradient-6.png";
+import gradient_7 from "../asset/gradient-7.png";
+import profil from "../asset/arief.png";
+import eth from "../asset/eth.png";
+import eth2 from "../asset/eth2.png";
+import eth3 from "../asset/eth3.png";
+import eth4 from "../asset/eth4.png";
+
+export default {
+  gradient_1,
+  gradient_2,
+  gradient_3,
+  gradient_4,
+  gradient_5,
+  gradient_6,
+  gradient_7,
+  profil,
+  eth,
+  eth2,
+  eth3,
+  eth4,
+};
